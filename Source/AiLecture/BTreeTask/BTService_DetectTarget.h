804b08fe4e6bdccf5f70897a70435b134fb32f72
MystIsle <pelhav@gmail.com>
2026-02-11 13:50:23 +0900

    [2부:03] Task 코드 생성

---
+++ Source/AiLecture/BTreeTask/BTService_DetectTarget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_DetectTarget.generated.h"
+
+UCLASS()
+class AILECTURE_API UBTService_DetectTarget : public UBTService
+{
+	GENERATED_BODY()
+
+public:
+	UBTService_DetectTarget();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetActorKey;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float DetectRadius = 1000.0f;
+};
---
+++ Source/AiLecture/BTreeTask/BTTask_FindRandomLocation.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_FindRandomLocation.generated.h"
+
+
+UCLASS()
+class AILECTURE_API UBTTask_FindRandomLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+	UBTTask_FindRandomLocation();
+	
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector PatrolLocationKey;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	float SearchRadius = 1000.0f;
+};
