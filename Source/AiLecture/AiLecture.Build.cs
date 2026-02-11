88f42ebd3682883d20eae0a8a5d0d5897778a706
MystIsle <pelhav@gmail.com>
2026-02-11 11:00:46 +0900

    [2부:01] GameplayTask 모듈 추가

--- Source/AiLecture/AiLecture.Build.cs
+++ Source/AiLecture/AiLecture.Build.cs
@@ -8,6 +8,17 @@ public class AiLecture : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+	        "Core", 
+	        "CoreUObject",
+	        "Engine", 
+	        "InputCore",
+	        "NavigationSystem",
+	        "AIModule",
+	        "Niagara",
+	        "EnhancedInput",
+	        "GameplayTasks",
+        });
     }
 }
